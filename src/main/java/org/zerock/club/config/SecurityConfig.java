fb2a70dd57b3d40ba3bcec25e5276b1dd3b5e935
hee-chan123 <sk9y9y@gmail.com>
2023-10-13 09:41:40 +0900

    10.13

--- src/main/java/org/zerock/club/config/SecurityConfig.java
+++ src/main/java/org/zerock/club/config/SecurityConfig.java
@@ -1,74 +1,90 @@
 package org.zerock.club.config;
 
+
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.zerock.club.security.handler.ClubLoginSuccessHandler;
+import org.zerock.club.security.service.ClubUserDetailsService;
 
-import javax.sql.DataSource;
-import java.util.List;
-import java.util.Map;
+import javax.servlet.http.HttpSession;
 
 @Configuration
-@EnableWebSecurity
 @Log4j2
-public class SecurityConfig{
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class SecurityConfig {
 
     @Autowired
-    private DataSource dataSource;
+    private ClubUserDetailsService userDetailsService;
 
     @Bean
-    PasswordEncoder passwordEncoder(){
+    PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
-        log.info("----------------------------filterChain-------------------------");
-        log.info("데이터소스: {}", dataSource);
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-        String sql = "select * from user_list where name = ?";
-        String username = "user";
-        Object[] params = new Object[]{username};
-        List<Map<String, Object>> result = jdbcTemplate.queryForList(sql, params);
-        System.out.println(result);
-        System.out.println(result.get(0).get("name").toString());
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService() {
+//        UserDetails user = User.builder()
+//                .username("user1")
+//                .password(passwordEncoder().encode("1111"))
+//                .roles("USER")
+//                .build();
+//
+//        log.info("userDetailsService............................");
+//        log.info(user);
+//
+//        return new InMemoryUserDetailsManager(user);
+//    }
 
-        UserDetails user = User.builder()
-                .username(result.get(0).get("name").toString())
-                .password(result.get(0).get("password").toString())
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(user);
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests((auth) -> {
+//            auth.antMatchers("/sample/all").permitAll();
+//            auth.antMatchers("/sample/member").hasRole("USER");
+            auth.antMatchers("/h2-console/**").permitAll();
+        });
+
+//        http.authorizeRequests()
+//                .antMatchers("/h2-console/**").permitAll();
+
+        http.csrf()
+                .ignoringAntMatchers("/h2-console/**")
+                .and().headers().frameOptions().sameOrigin();  // 여기!
+
+        http.formLogin();
+        http.csrf().disable();
+        http.logout(
+                logout -> logout
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .addLogoutHandler((request, response, authentication) -> {
+                    HttpSession session = request.getSession();
+                    session.invalidate();
+                })
+                .logoutSuccessHandler((request, response, authentication) -> response.sendRedirect("/login"))
+                .deleteCookies("remember-me")
+        );
+
+        http.oauth2Login().successHandler(successHandler());
+
+        http.rememberMe().tokenValiditySeconds(60*60*24*7).userDetailsService(userDetailsService);
+
+        return http.build();
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-//        log.info("----------------------------filterChain-------------------------");
-//        log.info("데이터소스: {}", dataSource);
-//        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-//        String sql = "select * from user_list where name = ?";
-//        String username = "user";
-//        Object[] params = new Object[]{username};
-//        List<Map<String, Object>> result = jdbcTemplate.queryForList(sql, params);
-//        System.out.println(result);
-        http.authorizeHttpRequests()
-                .antMatchers("/all/**").permitAll()
-                .antMatchers("/member/**").hasAnyRole("USER");
-        http.formLogin();
-        http.csrf().disable();
-        http.logout();
-        return http.build();
+    public ClubLoginSuccessHandler successHandler() {
+        return new ClubLoginSuccessHandler(passwordEncoder());
     }
 }
